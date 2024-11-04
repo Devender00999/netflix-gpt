d0f6998425463f979d0b1fb1e1588affbe73dadb
Devender Kumar <devender@rubick.ai>
2024-11-04 21:18:23 +0530

    Added login form fields

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import Logo from "../assets/logo.png";
 const Header = () => {
    return (
-      <div>
-         <img className="h-[80px] px-20" src={Logo} />
+      <div className="px-8 py-2  bg-gradient-to-b from-black">
+         <img className="w-44" src={Logo} />
       </div>
    );
 };
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,16 +5,38 @@ const Login = () => {
       <div className=" w-[100vw] h-[100vh] absolute top-0 z-[-2] bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/151f3e1e-b2c9-4626-afcd-6b39d0b2694f/web/IN-en-20241028-TRIFECTA-perspective_bce9a321-39cb-4cce-8ba6-02dab4c72e53_large.jpg)]">
          <div className="absolute w-[100vw] h-[100vh] z-[-1] bg-black opacity-50"></div>
          <Header />
-
-         <div className="w-full flex justify-center">
-            <div className="bg-[#00000079] w-max p-10 flex flex-col gap-7 rounded-md">
-               <h1 className="text-white text-4xl">Sign In</h1>
+         <div className="w-full flex justify-center ">
+            <div className="bg-[#00000099] w-max p-10 flex flex-col gap-7 rounded-md py-10">
+               <h1 className="text-white text-3xl font-bold">Sign In</h1>
                <div className="flex flex-col gap-5 ">
                   <input
-                     className="w-[350px] px-3 py-4 rounded-md bg-[#0908086b] border-red-100 border-[0.5px]"
+                     className="w-[350px] text-white px-3 py-4 rounded-[4px] bg-[#0908086b] border-gray-500 border-[0.5px]"
                      placeholder="Email or mobile number"
                   />
-                  <input className="w-[350px]" placeholder="Password" />
+                  <input
+                     className="w-[350px] text-white outline-none px-3 py-4 rounded-[4px] bg-[#0908086b] border-gray-500 border-[0.5px]"
+                     placeholder="Password"
+                  />
+                  <button className="w-[350px] bg-[#e50914] rounded-[4px] text-white px-4 py-2">
+                     Sign In
+                  </button>
+                  <span className="text-center text-[#dfdfdf] -mt-2">OR</span>
+                  <button className="w-[350px] bg-[#dfdfdf23] rounded-[4px] text-white px-4 py-2">
+                     Use a sign-in code
+                  </button>
+                  <a className="text-center cursor-pointer w-[350px] mt-[-15px] rounded-[4px] text-white px-4 py-2">
+                     Forgot Password?
+                  </a>
+                  <label className="text-white flex gap-2">
+                     <input type="checkbox" />
+                     <span>Remember me</span>
+                  </label>
+                  <div className="text-[#ffffffb3] -mt-3">
+                     New to netflix?
+                     <a className="text-center ml-2 cursor-pointer w-[350px] rounded-[4px] text-white py-2">
+                        Sign up now.
+                     </a>
+                  </div>
                </div>
             </div>
          </div>

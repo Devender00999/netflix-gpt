6e3d60f085fd0240950db61061833467cdabef1f
Devender Kumar <devender00999@gmail.com>
2024-11-05 22:04:17 +0530

    Testing something

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 const Login = () => {
    return (
       <div className=" w-[100vw] h-[100vh] absolute top-0 z-[-2] bg-[url(https://assets.nflxext.com/ffe/siteui/vlv3/151f3e1e-b2c9-4626-afcd-6b39d0b2694f/web/IN-en-20241028-TRIFECTA-perspective_bce9a321-39cb-4cce-8ba6-02dab4c72e53_large.jpg)]">
-         <div className="absolute w-[100vw] h-[100vh] z-[-1] bg-black opacity-50"></div>
+         <div className="absolute d w-[100vw] h-[100vh] z-[-1] bg-black opacity-50"></div>
          <Header />
          <div className="w-full flex justify-center ">
             <div className="bg-[#00000099] w-max p-10 flex flex-col gap-7 rounded-md py-10">
